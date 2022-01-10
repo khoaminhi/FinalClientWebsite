423155fac3895d56eed7b9c00a68f2e5b9c41e83
tqkhuong <quockhuongtran17@gmail.com>
2022-01-10 10:44:16 +0700

    fix mvc

--- components/productDetail/productDetailService.js
+++ components/productDetail/productDetailService.js
@@ -2,3 +2,14 @@ const {models} = require('../../models')
 exports.list = () => {
     return models.books.findAll({raw: true});
 }
+
+exports.findBook = async(id) => {
+    const book = await models.books.findOne({where: {id: id}, raw: true});
+    return book;
+}
+
+exports.findComments = async(id, page) => {
+    const itemPerPage = 2;
+    const comments = await models.comments.findAll({where: {bookid: id}, raw: true, offset: parseInt(page)*itemPerPage, limit: itemPerPage});
+    return comments;
+}
--- components/productList/productListService.js
+++ components/productList/productListService.js
@@ -1,5 +1,76 @@
-const {models} = require('../../models')
+const {models} = require('../../models');
+const books = require('../../models/books');
 
 exports.list = (page = 0, itemPerPage = 9) => {
     return models.books.findAll({offset: page*itemPerPage, limit: itemPerPage});
+}
+
+exports.listPages = async (page) => {
+    const itemPerPage = 9;
+    const books = await models.books.findAll({offset: parseInt(page)*itemPerPage, limit: itemPerPage});
+    return books;
+}
+
+exports.listCategories = async (categoryid) => {
+    const itemPerPage = 9;
+    const all_books = await models.books.findAll({raw: true, limit: itemPerPage});
+    let books = []
+    for (let i=0; i < all_books.length; i++)
+    {
+        if(all_books[i]['categoryid'] === categoryid)
+        {
+            books.push(all_books[i])
+        }
+    }
+    return books;
+}
+
+exports.listPublishers = async (publisherid) => {
+    const itemPerPage = 9;
+    const all_books = await models.books.findAll({raw: true, limit: itemPerPage});
+    let books = []
+    for (let i=0; i < all_books.length; i++)
+    {
+        if(all_books[i]['publisherid'] === publisherid)
+        {
+            books.push(all_books[i])
+        }
+    }
+    return books;
+}
+
+exports.listIncrease = async () => {
+    const itemPerPage = 9;
+    const all_books = await models.books.findAll({raw: true, limit: itemPerPage});
+    let books = all_books.sort(function(a,b) {
+        return b.price - a.price
+    });
+    return books;
+}
+
+exports.listDecrease = async () => {
+    const itemPerPage = 9;
+    const all_books = await models.books.findAll({raw: true, limit: itemPerPage});
+    let books = all_books.sort(function(a,b) {
+        return a.price - b.price
+    });
+    return books;
+}
+
+exports.listSearch = async (search) => {
+    const itemPerPage = 9;
+    const all_books = await models.books.findAll({raw: true, limit: itemPerPage});
+    let books = [];
+    for (let i=0; i < all_books.length; i++)
+    {
+        if(all_books[i]['categoryid'].includes(search))
+        {
+            books.push(all_books[i])
+        }
+        else if (all_books[i]['name'].includes(search))
+        {
+            books.push(all_books[i])
+        }
+    }
+    return books;
 }
