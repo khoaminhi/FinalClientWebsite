5bc6a16000e13e210858f8f5cd23643911ff1bd1
KhoaMinHi <minhkhoa031099@gmail.com>
2022-01-17 20:57:10 +0700

    tuning database string

--- models/index.js
+++ models/index.js
@@ -3,13 +3,13 @@ const { Sequelize } = require('sequelize');
 
 const initModels = require('./init-models');
 //const connPostgresString = process.env.DB_URL;//add require('dotenv').config(); into www
-//const connPostgresString = 'postgres://wxjfftihybqpun:584926c1c528115d979e1e2cd8ac86a41acc3da6b44eaefdb5be61662790a3de@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
-const connPostgresString = 'postgres://postgres:KJPAMI@localhost:5432/postgres';
+const connPostgresString = 'postgres://wxjfftihybqpun:584926c1c528115d979e1e2cd8ac86a41acc3da6b44eaefdb5be61662790a3de@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
+/*const connPostgresString = 'postgres://postgres:KJPAMI@localhost:5432/postgres';
 const sequelize = new Sequelize(connPostgresString, {
   dialect: 'postgres',
-});
+});*/
 
-/*
+
 const sequelize = new Sequelize(connPostgresString, {
     dialect: 'postgres',
     dialectOptions: {
@@ -18,7 +18,7 @@ const sequelize = new Sequelize(connPostgresString, {
         rejectUnauthorized: false
     }
     }
-  });*/
+  });
 
   
 // console.log(sequelize)
